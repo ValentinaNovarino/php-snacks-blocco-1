0332dd8a1746930c7612fcd1f851ac42e62e1a00
ValentinaNovarino <valenovarino@gmail.com>
2020-12-15 16:45:27 +0100

    iniziato snack2

---
+++ snack2/index.php
@@ -0,0 +1,9 @@
+<?php
+/*Passare 3 parametri GET: name, mail e age e verificare (cercando nella documentazione i metodi che non conosciamo) che:
+1. name sia più lungo di 3 caratteri,
+2. mail contenga un punto e una chiocciola
+3. age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”*/
+
+
+?>
