4488254a2122fb158aa82fafddd7f916bf8b0c2d
ValentinaNovarino <valenovarino@gmail.com>
2020-12-15 15:48:56 +0100

    iniziato snack1

---
+++ snack1/index.php
@@ -0,0 +1,11 @@
+<?php
+/*Creare un array $matches che contiene dei sotto-array, i quali rappresentano delle partite di basket.
+Ogni partita avrà le seguenti caratteristiche:
+* squadra di casa
+* squadra ospite
+* punti fatti dalla squadra di casa
+* punti fatti dalla squadra ospite
+L'obbiettivo è stampare a schermo tutte le partite.
+Ad esempio:
+Olimpia Milano - Cantù | 55 - 60*/
+?>
